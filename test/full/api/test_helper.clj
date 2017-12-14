c0f6431cdbbfa8d13fffbb77ba7f3a7cb163606a
Matt Elliott <mattdelliott@me.com>
2017-12-14 11:00:32 -0700

    Updated test helper and added in verification

--- test/full/api/test_helper.clj
+++ test/full/api/test_helper.clj
@@ -1,9 +1,17 @@
 (ns full.api.test-helper
     (:use mockery.core))
 
-(defn mock-http [r f]
+(defn with-mock-http [r f]
     (with-mock mock
         {:target :full.api.http/request
          :return (delay r)}
-         (f)
-         @mock))
+         {:result (f) :mock @mock }))
+
+(defn str-random [] (.toString (java.util.UUID/randomUUID)))
+
+(defn verify-mock [mock params & {:keys [call-count params]
+                                  :or   {call-count 1 params '()}}]
+    (and
+        (true? (:called? mock))
+        (= call-count (:call-count mock))
+        (every? #(> (.indexOf (:call-args mock) %) -1) params)))
